e3ee9d0a5f7494a4f54e0c2d35a65e05096bbf35
Ramees p <ramees.p@upgradjeet.com>
2022-08-09 15:06:27 +0530

    env added

--- App.tsx
+++ App.tsx
@@ -8,16 +8,20 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import useInterNetHandlingService from './src/services/internet-handling-service';
 import {store} from './src/store/configureStore';
 import {NavigationContainer} from '@react-navigation/native';
 import MainScreen from './src/screens/main-screen';
 import AppTheme from './src/theme/theme';
-
+import {ENV_VAR} from '@env';
 const App = () => {
   const {NoInterNetConnectionView} = useInterNetHandlingService();
+  // console.log(ENV_VAR);
+  useEffect(() => {
+    console.log(ENV_VAR);
+  }, []);
 
   return (
     <Provider store={store}>
