4046bf2f94549156766854061013f6159ed973b4
Ramees p <ramees.p@upgradjeet.com>
2022-09-14 13:30:51 +0530

    comments added

--- src/services/data-tracking-service.tsx
+++ src/services/data-tracking-service.tsx
@@ -4,6 +4,16 @@ interface dataObject {
   [key: string]: string;
 }
 //call this hook only from screens
+//This function is used with analytics it  will append user data with data you need to send
+/**
+ *
+ * @param data The data that you need to send with analytical data
+ * @param isUseUserDetails if you need user data to be appended with it make it true
+ * @returns {
+ *            dataToTrack : data to track
+ *            userDetails: it will return user details with current page that they are on
+ *          }
+ */
 const useDataTrackingService = (
   data: dataObject,
   isUseUserDetails: boolean,
