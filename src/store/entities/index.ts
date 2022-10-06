75c1a4525d7afaa14476e790f2b75163d42a250e
kapil_upgradjeet <kapil.shukla@upgradjeet.com>
2022-10-06 10:33:38 +0530

    merge changes

--- src/store/entities/index.ts
+++ src/store/entities/index.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import userPreferences from './user-preferences';
 import downloadManager from './downloadManager';
+import testExperience from './test-experience';
 
 export default combineReducers({
   userPreferences,
   downloadManager,
+  testExperience,
 });
